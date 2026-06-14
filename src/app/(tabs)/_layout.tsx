50aaac47e8e08d74c46b3b3e03ab6201d0937309
omarpulidom <95859005+omarpulidom@users.noreply.github.com>
2026-06-14 16:00:42 -0600

    chore: make tab bar float with transparent background

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Redirect, Tabs } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { View } from 'react-native'
 import { CustomTabBar } from '@/components/Elements/CustomTabBar'
 import { useAuth } from '@/components/Providers/AuthProvider'
 
@@ -18,12 +19,35 @@ export default function TabsLayout() {
         screenOptions={{
           headerShown: false,
           animation: 'shift',
+          tabBarShowLabel: false,
+          tabBarBackground: () => <View />,
+          tabBarStyle: {
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'transparent',
+            borderTopWidth: 0,
+            elevation: 0,
+          },
         }}
       >
         <Tabs.Screen
           name='index'
           options={{
             title: 'Home',
+          }}
+        />
+        <Tabs.Screen
+          name='groups'
+          options={{
+            title: 'Groups',
+          }}
+        />
+        <Tabs.Screen
+          name='checkin'
+          options={{
+            title: 'New Checkin',
           }}
         />
         <Tabs.Screen
