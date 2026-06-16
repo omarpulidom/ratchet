08a5fc2635b1dd4d29d636cc73b6077c3677d94b
omarpulidom <95859005+omarpulidom@users.noreply.github.com>
2026-06-15 23:58:12 -0600

    feat: integrate theme toggle in profile screen

--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,13 +1,19 @@
-import { Text, TouchableOpacity } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { ThemeToggle } from '@/components/Elements/ThemeToggle'
 import { useAuth } from '@/components/Providers/AuthProvider'
 
 export default function ProfileTab() {
   const { logout } = useAuth()
   return (
-    <SafeAreaView className='flex-1 items-center justify-center bg-secondary'>
-      <Text className='text-secondary-900 tracking-tighter'>Profile Tab</Text>
-      <TouchableOpacity onPress={() => logout()} className='mt-4 bg-primary-300 p-2 rounded-lg'>
+    <SafeAreaView className='flex-1 bg-secondary p-4'>
+      <View className='gap-2 mb-6'>
+        <Text className='text-secondary-700 font-poppins-regular text-[14px] tracking-tighter pl-4'>
+          Tema
+        </Text>
+        <ThemeToggle />
+      </View>
+      <TouchableOpacity onPress={() => logout()} className='bg-primary-300 p-2 rounded-lg self-start'>
         <Text className='text-primary tracking-tighter'>Log-out</Text>
       </TouchableOpacity>
     </SafeAreaView>
